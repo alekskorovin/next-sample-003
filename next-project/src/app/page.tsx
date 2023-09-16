7128f475d81973b2c50e114a25df6caee68a4b05
Dor Shinar <dors718@gmail.com>
2023-09-16 21:58:46 +0300

    init

--- next-project/src/app/page.tsx
+++ next-project/src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Button, Dialog } from "example-package";
+import { Button } from "example-package";
 
 export default function Home() {
   const [count, setCount] = useState(0);
